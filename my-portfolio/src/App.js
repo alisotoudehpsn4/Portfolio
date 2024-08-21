88256158ae5af6aecd8e07cb927d80ba96105747
Alisotoudehpsn4 <alisotoudehpsn4@gmail.com>
2024-08-21 20:10:29 +1000

    updated header and major css

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import './assets/styles.css';
+import './index.css'; 
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Articles from './components/Article';
 import Footer from './components/Footer';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <Header />
       <Sidebar />
       <main className="content">
--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -8,10 +8,43 @@ function Header() {
       <div className="header-container">
         <img src={profilePicture} alt="Ali Sotoudeh" className="profile-pic" />
         <h1>Ali Sotoudeh</h1>
-        <p className="subtitle">Software Developer</p>
-        <p className="tagline">
-          <i className="fas fa-laptop-code"></i> Passionate about creating impactful digital solutions.
-        </p>
+        <p className="tagline">Software Developer</p>
+        <p className="secondary-tagline">Passionate about creating impactful digital solutions.</p>
+        <div className="contact-info">
+          <a href="tel:+61434031715" className="contact-item">
+            <i className="fas fa-phone"></i>
+          </a>
+          <a 
+            href="https://www.google.com/maps/place/St+Kilda,+Melbourne,+Australia" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="contact-item"
+          >
+            <i className="fas fa-map-marker-alt"></i>
+          </a>
+          <a 
+            href="https://www.linkedin.com/in/ali-sotoudeh-0aaa562b9" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="contact-item"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a 
+            href="mailto:Alisotoudehpsn4@gmail.com" 
+            className="contact-item"
+          >
+            <i className="fas fa-envelope"></i>
+          </a>
+          <a 
+            href="https://github.com/alisotoudehpsn4" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="contact-item"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
       </div>
     </header>
   );
