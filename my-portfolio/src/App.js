9b43298850e69d38ec79515a0858f84eaf304cb1
Alisotoudehpsn4 <alisotoudehpsn4@gmail.com>
2024-08-21 23:16:08 +1000

    update

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css'; 
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
@@ -6,10 +6,20 @@ import Articles from './components/Article';
 import Footer from './components/Footer';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Set the loaded state to true after the initial render
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 100); // Short delay to ensure the animation starts after the components are rendered
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="app-container">
+    <div className={`app-container ${isLoaded ? 'loaded' : ''}`}>
       <Header />
       <Sidebar />
       <main className="content">
